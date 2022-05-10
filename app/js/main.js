59d959222c534051c9e4457eee88e450e3b3fd45
git config --global core.autocrlf true <kosslanoss@gmail.com>
2022-05-10 23:29:57 +0300

    fixed header-catalog

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function(){
 
+  $('.header-bottom__catalog-btn').on('click', function(){
+    $('.header-bottom__catalog-list').toggleClass('header-bottom__catalog-list--active');
+    $('.header-bottom__catalog-btn').toggleClass('header-bottom__catalog-btn--active');
+  });
+
 });
