9a783240ce109b8fdc5807b934c6717859a9159a
git config --global core.autocrlf true <kosslanoss@gmail.com>
2022-05-14 22:44:49 +0300

    fixed product

--- app/js/main.js
+++ app/js/main.js
@@ -6,12 +6,14 @@ $(function(){
   });
 
   $('.hero__slider').slick({
-  dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  adaptiveHeight: true,
-  infinite: false
-});
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    infinite: false
+  });
+
+  var mixer = mixitup('.product__items');
 
 });
